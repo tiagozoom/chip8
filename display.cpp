252b9e3675aa9b0eeafc51465afa6d8762f47812
tiago <tiagozhk@gmail.com>
2019-09-17 18:19:11 -0400

    Implementing display

--- display.cpp
+++ display.cpp
@@ -3,12 +3,19 @@
 #include <SDL2/SDL.h>
 using namespace std;
 
-#define W 320
-#define H 240
+#define W 64
+#define H 32
 
 int main(int argc, char *argv[]){
     Uint32 pixels[W * H] = {
-        0xFFFF0000
+        0xFF000000,
+        0x00000000,
+        0xFF000000,
+        0xFF000000,
+        0xFF000000,
+        0x00000000,
+        0xFF000000,
+        0x00000000
     };
 
     SDL_Rect rect;
@@ -18,7 +25,7 @@ int main(int argc, char *argv[]){
         return 1;
     };
 
-    SDL_Window* window = SDL_CreateWindow("testerino", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, W, H, SDL_WINDOW_RESIZABLE);
+    SDL_Window* window = SDL_CreateWindow("testerino", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE);
     if(window == nullptr) {
         cout << "Error initializing SDL window" << endl;
         SDL_Quit();
@@ -43,20 +50,33 @@ int main(int argc, char *argv[]){
 
     SDL_Event event;
     bool quit = false;
+
     while (!quit){
-        SDL_UpdateTexture(texture, nullptr, pixels, W);
+        SDL_UpdateTexture(texture, nullptr, pixels, 64 * 4);
         SDL_WaitEvent(&event);
+	int teste[6] = { 0xBA, 0x7C, 0xD6, 0xFE, 0x54, 0xAA};
+	int width = 64;
+	for(int j = 0; j < 6; j++, width += 64){
+		for(int i = 7; i >= 0; i--){
+		       pixels[7 - i + width] = ((teste[j] >> i) & 0x1) ? 0xFF000000 : 0xFFFFFFFF ;
+		}
+	}
+	/*pixels[0] = 0xFF000000;
+        pixels[1] = 0xFFFFFFFF;
+        pixels[2] = 0xFF000000;
+        pixels[3] = 0xFF000000;
+        pixels[4] = 0xFF000000;
+        pixels[5] = 0xFFFFFFFF;
+        pixels[6] = 0xFF000000;
+        pixels[7] = 0xFFFFFFFF;*/
+
+        SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+        SDL_RenderPresent(renderer);
         switch(event.type){
-            case SDL_MOUSEMOTION:
-                pixels[0] = 0x0;
-                break;
             case SDL_QUIT:
                 quit = 1;
                 break;
         }
-
-        SDL_RenderCopy(renderer, texture, nullptr, nullptr);
-        SDL_RenderPresent(renderer);
     }
     return 0;
 }
